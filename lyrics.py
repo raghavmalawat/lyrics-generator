cc4ce36385743c130dc382f0d7f901ba9616e8f8
raghavmalawat <raghavmalawat@gmail.com>
2019-04-24 21:01:58 +0530

    Coded

---
+++ lyrics.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+import os
+
+
+def show_lyrics():
+    # taking input of artist and track from user
+    artist = input("enter the name of the ARTIST : ")
+    artist = artist.lower().replace(" ","-")
+    track = input("enter the name of the TRACK : ")
+    track = track.lower().replace(" ","-")
+    # creating url
+    url = 'http://www.metrolyrics.com/' + track + '-lyrics-' + artist + '.html'
+    # getting contents of the given url
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content,"lxml")
+    # scrapping lyrics from all the contents of the webpage
+    lyrics = soup.find_all("p", {"class":"verse"})
+    # printing the lyrics after converting
+    print("\nHere goes the lyrics:\n")
+    for i in lyrics:
+        print(i.text)
+
+
+def main():
+    show_lyrics()
+
+
+if __name__ == "__main__":
+    print("initialising")
+    main()
